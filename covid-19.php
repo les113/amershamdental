df7e1ce3317c9fdd4db88dbf0d248d6f300b2124
les113 <leslumb@yahoo.co.uk>
2020-06-18 15:42:27 +0100

    covid-19 amend

--- covid-19.php
+++ covid-19.php
@@ -1,16 +1,20 @@
 <div class="row">
 	<div class="offset_2 grid_8"   style="text-align:center;">			
-		<h1>COVID-19 Update 30th March 2020</h1>
-		<h2>Amersham Hill Dental Clinic - Dental Emergencies Only</h2>
+		<h2 style="line-height:1.2em;">COVID-19 Update 8th June 2020<br/>
+		 Amersham Hill Dental Clinic - Dental Emergencies Only</h2>
 		<div class="covid19Msg">
-			<h3>Following guidelines from NHS England and the British Dental Association we have closed the practice temporarily. We will be operating a remote emergency service only.<h3>
-			<p><strong>If you have a dental emergency, please phone</strong></p>
-			<h4>01494 526392 or 07623984829</h4>
+			<p>As from the 8th June 2020 the Surgery will be open in a limited capacity.
+			We are currently seeing Emergency patients for Assessments and Advice only.
+			Ensuring the safety of our patients and staff is our priority and of upmost importance.
+			We have been working hard over the last few weeks preparing for re-opening the practice, however due to lack of available PPE we will be operating in a limited capacity until we have a good reliable supply of these essential items.
+			</p>
+			<h3>If you have a dental emergency, please phone</h3>
+			<h4>01494 526392</h4>
 			<p>We want to reassure our patients the health and safety of our patients and staff at Amersham Hill Dental Clinic is our top priority. We are closely monitoring the Coronavirus (COVID-19) following Government and Public Health England guidelines. More information and advice can be found on the following NHS website <br/>
 			<a href="https://www.nhs.uk/conditions/coronavirus-covid-19" target="_blank">www.nhs.uk/conditions/coronavirus-covid-19</a></p>
 			<p>From everyone in the team at Amersham Hill Dental Clinic, we would like to thank you for you co operation and support during this difficult period. We will continue to keep you updated on our website.</p>
 		</div>
 		<p><a href="#" class="openCovid19" style="display:none;">Read More</a></p>
 	</div>
-</div>	
+</div>
 
