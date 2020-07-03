826a4d009316713ab6997ad97782f7489611b29f
les113 <leslumb@yahoo.co.uk>
2020-07-03 16:01:45 +0100

    covid-19 patient process added

--- covid-19.php
+++ covid-19.php
@@ -6,13 +6,21 @@
 			<p>As from the 8th June 2020 the Surgery will be open in a limited capacity.
 			We are currently seeing Emergency patients for Assessments and Advice only.
 			Ensuring the safety of our patients and staff is our priority and of upmost importance.
-			We have been working hard over the last few weeks preparing for re-opening the practice, however due to lack of available PPE we will be operating in a limited capacity until we have a good reliable supply of these essential items.
+			We have been working hard over the last few weeks preparing for re-opening the practice, 
+			however due to lack of available PPE we will be operating in a limited capacity until 
+			we have a good reliable supply of these essential items.
 			</p>
 			<h3>If you have a dental emergency, please phone</h3>
 			<h4>01494 526392</h4>
-			<p>We want to reassure our patients the health and safety of our patients and staff at Amersham Hill Dental Clinic is our top priority. We are closely monitoring the Coronavirus (COVID-19) following Government and Public Health England guidelines. More information and advice can be found on the following NHS website <br/>
+			<p>We want to reassure our patients the health and safety of our patients and 
+		    staff at Amersham Hill Dental Clinic is our top priority.</p>
+			<p>We are closely monitoring the Coronavirus (COVID-19) following Government 
+			and Public Health England guidelines. More information and advice can be found on the following NHS website <br/>
 			<a href="https://www.nhs.uk/conditions/coronavirus-covid-19" target="_blank">www.nhs.uk/conditions/coronavirus-covid-19</a></p>
-			<p>From everyone in the team at Amersham Hill Dental Clinic, we would like to thank you for you co operation and support during this difficult period. We will continue to keep you updated on our website.</p>
+			<p>From everyone in the team at Amersham Hill Dental Clinic, we would like to thank you for you co operation and support 
+			during this difficult period. We will continue to keep you updated on our website.</p>
+			<p>Please read about our covid-19 appointment process <a href="PT_Journey.pdf" target="_blank">here</a>.
+			</p>
 		</div>
 		<p><a href="#" class="openCovid19" style="display:none;">Read More</a></p>
 	</div>
